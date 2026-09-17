b2e19870222a7c790290471e2f1e77f3f90a724a
pollobkhalil <pollobkhalilsp@gmail.com>
2026-09-17 17:47:47 +0600

    Fix case sensitivity for Home and Movies imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/home';
-import Movies from './pages/movies';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
 
 
 
